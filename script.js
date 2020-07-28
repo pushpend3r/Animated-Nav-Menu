5bea0a7e14fe5a88f33efd6f6db571a30a4da21b
Pushpender Singh <piyushthakur5678@gmail.com>
2020-07-28 15:50:28 +0530

    First Commit

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 let navToggler = document.querySelector(".nav-toggler");
 let nav = document.querySelector("nav");
+let strip = document.querySelectorAll(".strip");
+let side = document.querySelectorAll(".side");
+let navLinks = document.getElementById("nav-links");
 
 navToggler.addEventListener("click", () => {
-  if (nav.classList.contains("up")) nav.classList.replace("up", "down");
-  else nav.classList.replace("down", "up");
+  if (nav.classList.contains("up")) {
+    nav.classList.replace("up", "down");
+    strip.forEach((s, index) => {
+      s.classList.replace(
+        `strip-${index + 1}-trans-from`,
+        `strip-${index + 1}-trans-to`
+      );
+    });
+
+    side.forEach((s, index) => {
+      s.classList.replace(`side-${index + 1}-from`, `side-${index + 1}-to`);
+    });
+
+    navLinks.classList.replace("nav-links-from", "nav-links-to");
+
+  } else {
+    nav.classList.replace("down", "up");
+    strip.forEach((s, index) => {
+      s.classList.replace(
+        `strip-${index + 1}-trans-to`,
+        `strip-${index + 1}-trans-from`
+      );
+    });
+
+    side.forEach((s, index) => {
+      s.classList.replace(`side-${index + 1}-to`, `side-${index + 1}-from`);
+    });
+
+    navLinks.classList.replace("nav-links-to", "nav-links-from");
+  }
 });
